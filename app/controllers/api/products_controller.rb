00f895c10dc901635334fd5076b05c48a849337a
kristinreddington <kristinreddington@gmail.com>
2018-08-12 20:02:58 -0400

    Finish api/controller methods

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,48 @@
-class Api::Products < ApplicationController
-  
+class Api::ProductsController < ApplicationController
 
+  before_action :set_product, :only => [:show, :update, :destroy]
+
+  def index
+    render json: Product.all
+  end
+
+  def show
+    render json: @product
+  end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      render json: product
+    else
+      render json: {:message => product.errors }, :status => 400
+    end
+  end
+
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: { :message => product.errors }, :status => 400
+    end
+  end
+
+  def destroy
+    if @product.destroy
+      render json: { :message => "Successfully Deleted!" }, :status => 204
+    else
+      render json: { :message => "Unable to Delete Product" }, :status => 400
+    end
+  end
+
+  private
+
+  def set_product
+    @product = Product.find_by(:id => params[:id])
+  end
+
+  def product_params
+     params.require(:product).permit(:name, :price, :image_url, :description)
+  end
 
 end
