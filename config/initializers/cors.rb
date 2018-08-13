1a4d844fa90b2a179217ad5b3387da9c841aa7df
kristinreddington <kristinreddington@gmail.com>
2018-08-13 17:07:33 -0400

    Add user model

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,7 +6,7 @@
 # Read more: https://github.com/cyu/rack-cors
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do  allow do
-     origins 'localhost:3001'
+     origins 'localhost:3000'
 
      resource '*',
        headers: :any,
