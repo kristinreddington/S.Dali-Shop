e1f09f2b3152a591e30ea6d5d9d3ac9934593b26
Kristin Reddington <kristinreddington@gmail.com>
2024-05-23 16:19:10 -0400

    add rake db:migrate to Procfile

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative 'application'
 
+print 'secret_key_base:', Rails.application.credentials.secret_key_base
 # Initialize the Rails application.
 Rails.application.initialize!
