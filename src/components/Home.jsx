bb9b61a64deb343b4ecb59f1a0784c11bdffa711
Kristin Reddington <kristinreddington@gmail.com>
2024-08-02 10:32:42 -0400

    add params to checkout_controller, change seed data, change logo, change fonts, remove conditional menu for large screens, update css in product cart

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className='w-100% mt-[30px] w-full mx-auto py-4 text-center flex flex-col justify-center'>
       <p className='font-face-tenor text-dim-gray font-bold p-2'>Not just retail.</p>
-      <h1 className='font-face-lemurmure text-dim-gray md:text-5xl sm:text-3xl text-3xl md:py-4'>
+      <h1 className='font-face-lemurmure text-black text-opacity-70 md:text-5xl sm:text-3xl text-3xl md:py-4'>
       Immerse Yourself Into the Dali Dimmension</h1>
       <div className='flex justify-center items-center'>
         <p className='font-face-tenor text-dim-gray text-xl font-bold py-4 px-2 md:px-0'>Ethereal, Classic, Sustainable,</p>
