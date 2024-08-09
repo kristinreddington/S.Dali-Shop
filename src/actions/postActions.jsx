70c8d47147539e44a99f7f27f1cf18831636f315
Kristin Reddington <kristinreddington@gmail.com>
2024-08-09 11:27:22 -0400

    fix tumblr api get request for blog page, hide nav when selecting nav item

--- src/actions/postActions.jsx
+++ src/actions/postActions.jsx
@@ -1,13 +1,13 @@
-import $ from 'jquery'
+import $ from "jquery";
 
 // ** Async Actions ** //
 
-const setPosts = posts => {
+const setPosts = (posts) => {
   return {
-    type: 'GET_POSTS_SUCCESS',
-    posts
-  }
-}
+    type: "GET_POSTS_SUCCESS",
+    posts,
+  };
+};
 
 // export const getPosts = (offset) => {
 //   return (dispatch) => {
@@ -35,28 +35,31 @@ let posts = [];
 let postsTwo = [];
 export const getPosts = () => {
   return (dispatch) => {
-  let limit = 50; // set this to any value you want up to 50. 
-  const retrieveMore = function (offset) {
+    let limit = 50; // set this to any value you want up to 50.
+    const retrieveMore = function (offset) {
       $.ajax({
-          url: `https://api.tumblr.com/v2/blog/somewhatadrift.tumblr.com/posts/photo?offset=${offset}&limit=${limit}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1534134287&oauth_nonce=vB3KXh&oauth_version=1.0&oauth_signature=l3UOWPW1GXHI616cQ6enosymB8k=&api_key=fZ6DymUqbEteBsS6GGvexSWDuE8AtLgPSOjjYWxx6OHwJU1Ww8`,
-          type: "GET",
-          context: this,
-          success: function (result) {
-              const totalPosts = result.response.total_posts;
-              const postLength = result.response.posts.length;
-                const filteredPosts = result.response.posts.filter(post => post.type === 'photo');
-                console.log(filteredPosts);
-                filteredPosts.forEach(post => postsTwo.push(post));
-                  console.log(postsTwo);
-              if (totalPosts === offset && postLength == 0) {
-                  retrieveMore(offset + limit);
-              } else {
-                posts = postsTwo;
-                dispatch(setPosts(posts));
-              }
+        url: `https://api.tumblr.com/v2/blog/somewhatadrift.tumblr.com/posts/photo?offset=${offset}&limit=${limit}&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1534134287&oauth_nonce=vB3KXh&oauth_version=1.0&oauth_signature=l3UOWPW1GXHI616cQ6enosymB8k=&api_key=fZ6DymUqbEteBsS6GGvexSWDuE8AtLgPSOjjYWxx6OHwJU1Ww8`,
+        type: "GET",
+        context: this,
+        success: function (result) {
+          console.log(result);
+          const totalPosts = result.response.total_posts;
+          const postLength = result.response.posts.length;
+          const filteredPosts = result.response.posts.filter(
+            (post) => post.type === "photo"
+          );
+          filteredPosts.forEach((post) => postsTwo.push(post));
+          if (offset < totalPosts) {
+            offset += 51;
+            console.log(offset);
+            return retrieveMore(offset);
+          } else {
+            posts = postsTwo;
+            dispatch(setPosts(posts));
           }
+        },
       });
+    };
+    retrieveMore(0);
   };
-  retrieveMore(0);
-}
 };
