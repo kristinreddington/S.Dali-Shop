d63f6679c244cbd8fc90ff35e5cc3913ac8a43e1
Kristin Reddington <kristinreddington@gmail.com>
2024-08-22 19:49:08 -0400

    create render.yaml file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,11 +5,10 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
- Rails.application.config.middleware.insert_before 0, Rack::Cors do  allow do
-     origins 'localhost:3000'
-
-     resource '*',
-       headers: :any,
-       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+ Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+     origins 'http://localhost:3000'
+     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+     credentials: true
    end
  end
