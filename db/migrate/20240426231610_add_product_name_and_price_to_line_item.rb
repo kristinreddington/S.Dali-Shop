4574c44b2d93b2299542c5323949309c1c4ac1f6
Kristin Reddington <kristinreddington@gmail.com>
2024-08-22 20:44:48 -0400

    remove old migrate files

--- db/migrate/20240426231610_add_product_name_and_price_to_line_item.rb
+++
@@ -1,8 +0,0 @@
-class AddProductNameAndPriceToLineItem < ActiveRecord::Migration[7.1]
-  def change
-    add_column :line_items, :product_name, :string
-    add_column :line_items, :product_price, :integer
-    add_column :line_items, :product_description, :string
-    add_column :line_items, :product_image, :string 
-  end
-end
--- db/migrate/20240506211040_drop_table.rb
+++
@@ -1,4 +0,0 @@
-class DropTable < ActiveRecord::Migration[7.1]
-  def change
-  end
-end
