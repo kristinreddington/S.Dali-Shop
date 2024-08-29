72e0dabf50307332efea429faa7651a6dcbdddd5
Kristin Reddington <kristinreddington@gmail.com>
2024-08-29 11:54:02 -0400

    push render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,4 +11,4 @@ bundle exec rails assets:clean
 # perform database migrations in the build command.
 # Uncomment the following line:
 
-bundle exec rails db:migrate
+bundle exec rails db:{migrate,seed}
