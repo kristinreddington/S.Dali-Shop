fa8943d9786886ac5bc79da32ebf51c554ff2a3a
Kristin Reddington <kristinreddington@gmail.com>
2024-08-29 12:16:21 -0400

    add render production url to cors.rb and to session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,4 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "http://localhost:3000"
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://s-dali-shop.onrender.com"
+
+# for dev until I find workaround
+# Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "http://localhost:3000"
